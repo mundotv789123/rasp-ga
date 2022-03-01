3fe6d98a326a0d3f47319640977775d97912996e
mundotv789123 <gamesminecraftonline@gmail.com>
2022-03-01 16:07:23 -0300

    Update

--- migrations/001-create_tables.sql
+++ migrations/001-create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `raspga_links` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name`  VARCHAR(64) UNIQUE NOT NULL,
-    `url`  VARCHAR(128) UNIQUE NOT NULL,
+    `url`  VARCHAR(128) NOT NULL,
     `ip_address` VARCHAR(64) DEFAULT NULL,
     `created_at` DATETIME NOT NULL DEFAULT NOW(),
     PRIMARY KEY(`id`)
