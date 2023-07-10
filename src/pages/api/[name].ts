4f9ff45d5729ee23125e77959ed185a81631dd93
mundotv789123 <789michaelfernandes789@gmail.com>
2023-07-10 07:07:17 -0300

    corrigindo back-end

--- src/pages/api/[name].ts
+++ src/pages/api/[name].ts
@@ -1,6 +1,11 @@
-import mysql from 'serverless-mysql';
-import validuri from 'valid-url';
+import mysql, { ServerlessMysql } from 'serverless-mysql';
 import url from 'url'
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface Link {
+    id: number,
+    url: string
+}
 
 async function getDB() {
     var conn = mysql({
@@ -15,7 +20,7 @@ async function getDB() {
     return conn;
 }
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let db = await getDB();
     if (db === null) {
         res.status(500).json({message: 'internal error'});
@@ -32,10 +37,10 @@ export default async function handler(req, res) {
     res.status(405).json({message: 'method is not supported'});
 }
 
-async function handlerGet(req, res, db) {
-    let results = await db.query('SELECT `id`, `url` FROM `raspga_links` WHERE `name` = ?', [req.query.name]);
+async function handlerGet(req: NextApiRequest, res: NextApiResponse, db: ServerlessMysql) {
+    let results = await db.query<Array<Link>>('SELECT `id`, `url` FROM `raspga_links` WHERE `name` = ?', [req.query.name]);
     if (results.length === 0) {
-        res.status(404).send();
+        res.status(404).send("");
         return;
     }
     /* registrando acesso */
@@ -45,10 +50,14 @@ async function handlerGet(req, res, db) {
     res.status(200).json({url: results[0].url})
 }
 
-async function handlerPost(req, res, db) {
+async function handlerPost(req: NextApiRequest, res: NextApiResponse, db: ServerlessMysql) {
     /* verificando o nome */
     if (req.query.name.length > 32) {
         res.status(400).json({message: 'E nome não pode conter mais que 32 caracteres'});
+        return;
+    }
+
+    if (typeof req.query.name !== 'string') {
         return;
     }
 
@@ -63,23 +72,24 @@ async function handlerPost(req, res, db) {
         return;
     }
 
-    if (!req.body.url_link.length > 10000) {
+    if (req.body.url_link.length > 10000) {
         res.status(400).json({message: 'URL não pode ser maior que 10000 caracteres'});
         return;
     }
 
-    if (!validuri.isUri(req.body.url_link)) {
+    if (!RegExp('^(https?:\\/\\/)([a-zA-Z0-9]{1,255}(\\.[a-zA-Z0-9]{1,20}){1,4})(\\/[a-zA-Z0-9\\-]{0,255}){0,25}(\\?[a-zA-Z0-9]{0,255}\\=[^\\=&]{0,32}(&[a-zA-Z0-9]{0,255}\\=[^\\=&]{0,255}){0,20})?$').test(req.body.url_link)) {
         res.status(400).json({message: 'Link informado é inválido'});
         return;
     }
     
-    let blacklist = await db.query('SELECT `hostname` FROM `raspga_url_blacklist` WHERE `hostname` = ?', [url.parse(req.body.url_link).hostname]);
+    let url_parse = new URL(req.body.url_link);
+    let blacklist = await db.query<String[]>('SELECT `hostname` FROM `raspga_url_blacklist` WHERE `hostname` = ?', [url_parse.hostname]);
     if (blacklist.length > 0) {
         res.status(400).json({message: 'Essa URL não pode ser encurtado'});
         return;
     }
 
-    let results = await db.query('SELECT `url` FROM `raspga_links` WHERE `name` = ?', [req.query.name]);
+    let results = await db.query<Array<Link>>('SELECT `id`, `url` FROM `raspga_links` WHERE `name` = ?', [req.query.name]);
     if (results.length > 0) {
         res.status(400).json({message: 'O nome já está em uso'});
         return;
@@ -87,5 +97,5 @@ async function handlerPost(req, res, db) {
     
     let ip = (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'] : req.connection.remoteAddress);
     await db.query('INSERT INTO `raspga_links` (`name`, `url`, `ip_address`) VALUES (?, ?, ?)', [req.query.name, req.body.url_link, ip]);
-    res.status(200).send();
+    res.status(200).send("");
 }
